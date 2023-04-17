cd6eabd491bf1c5ce48e580e8bcabde963f98594
zhuxiuwei <zhuxiuwei@meituan.com>
2023-04-17 23:44:46 +0800

    p8:用Spring的RestTemplate，发送http远程服务调用请求。

--- cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
@@ -13,4 +14,13 @@ public class OrderApplication {
         SpringApplication.run(OrderApplication.class, args);
     }
 
+    /**
+     * 视频P08：创建RestTemplate并注册到Spring容器
+     * 用于发送rest请求来进行远程调用。
+     * @return
+     */
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
