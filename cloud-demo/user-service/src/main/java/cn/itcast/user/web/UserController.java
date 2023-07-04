86f7d13b9bce9d47559d03f4f8191cc3785bf728
zhuxiuwei <zhuxiuwei@meituan.com>
2023-07-04 14:42:39 +0800

    p26 微服务读取nacos配置

--- cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -4,7 +4,11 @@ import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
@@ -13,6 +17,15 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    // <p26 微服务读取nacos配置>: 测试nacos配置文件读取
+    @Value("${pattern.dateformat}")
+    private String dateformat;
+    @GetMapping("/now")
+    public String now() {
+        System.out.println(dateformat);
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+    }
 
     /**
      * 路径： /user/110
